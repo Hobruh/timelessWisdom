29f20e757dde456b4c4a0c821fa653ff670e5465
Adrian Gharineh <adrian.ghari@gmail.com>
2024-09-02 15:13:35 -0500

    Added more quotes

--- script2.js
+++ script2.js
@@ -3,25 +3,29 @@ const generateButton = document.getElementById("generate-button");
 const imgElement = document.getElementById("img-element");
 const body = document.getElementById("body")
 
+
 function generateRandomImage(imageArray, imgElement) {
-  // Get a random index from the image array
-  const randomIndex = Math.floor(Math.random() * imageArray.length);
-
-  // Get the random image URL from the array
-  const randomImage = imageArray[randomIndex];
-
-  // Update the `src` attribute of the image element with the correct path
-  imgElement.src = randomImage;
-
-  // Show the image (optional)
-  imgElement.style.display = "flex"; // This ensures the image is displayed
-  imgElement.style.width = "600px"; // Adjust the width as needed
-  imgElement.style.height = "600px"; // Adjust the height as needed
-  imgElement.style.objectFit = "cover"; // Maintain aspect ratio and cover the container
-  // Move the elements up when the button is clicked
-  imgElement.style.top = "100px";
-  generateButton.style.top = "850px";
-}
+    // Get a random index from the image array
+    const randomIndex = Math.floor(Math.random() * imageArray.length);
+  
+    // Get the random image URL from the array
+    const randomImage = imageArray[randomIndex];
+  
+    // Update the `src` attribute of the image element with the correct path
+    imgElement.src = randomImage;
+  
+    // Show the image (optional)
+    imgElement.style.display = "flex"; // This ensures the image is displayed
+    imgElement.style.width = "600px"; // Adjust the width as needed
+    imgElement.style.height = "600px"; // Adjust the height as needed
+    imgElement.style.objectFit = "cover"; // Maintain aspect ratio and cover the container
+    // Move the elements up when the button is clicked
+    imgElement.style.top = "100px";
+    generateButton.style.top = "850px";
+    // ...
+messageContainer.style.display = "inline-block";
+messageContainer.style.opacity = 1;
+  }
 
 generateButton.addEventListener("click", () => generateRandomImage(imageArray, imgElement));
 
@@ -33,4 +37,8 @@ const imageArray = [
   "Resources/Images/osho.jpg",
   "Resources/Images/guruGobind.jpg",
   "Resources/Images/yogananda.jpg",
+  "Resources/Images/sriM.jpg",
+  "Resources/Images/guruNanak.jpg",
+  "Resources/Images/saiBaba.jpg",
+  "Resources/Images/srisriRavi.jpg"
 ];
